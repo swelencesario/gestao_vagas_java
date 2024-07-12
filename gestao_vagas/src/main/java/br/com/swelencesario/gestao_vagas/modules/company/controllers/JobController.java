e8a0dc04807f98e6bba8475e996b373c2ddc26b8
Swelen <swelens2@hotmail.com>
2024-07-12 12:55:45 -0300

    tentativa de modificar a / do endpoint /job (sem sucesso)

--- gestao_vagas/src/main/java/br/com/swelencesario/gestao_vagas/modules/company/controllers/JobController.java
+++ gestao_vagas/src/main/java/br/com/swelencesario/gestao_vagas/modules/company/controllers/JobController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/job/")
+@RequestMapping("/job")
 public class JobController {
 
     @Autowired
